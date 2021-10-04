1808aabe9f5ba8b57440ce559dc41f3dafcc16b9
Marsen <admin@marsen.me>
2021-10-04 10:49:13 +0800

    hexo g

--- themes/landscape/layout/_partial/likecoin.js
+++
@@ -1,7 +0,0 @@
-<div id="likecoin">
-  <script type="text/javascript">
-    document.write(
-      "<iframe scrolling='no' frameborder='0' sandbox='allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox allow-storage-access-by-user-activation' style='height: 212px; width: 100%;' src='https://button.like.co/in/embed/marsenlin/button?referrer=" +
-      encodeURIComponent(location.href.split("?")[0].split("#")[0]) + "'></iframe>");
-  </script>
-</div>
